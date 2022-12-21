9c936e33baa02203f6605df506c243cf8563641d
sixdgt <c4crypt@gmail.com>
2022-12-22 00:27:49 +0545

    expense add and index page

--- expenseswebsite/expenses/views.py
+++ expenseswebsite/expenses/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
+from django.contrib import messages
+
 # Create your views here.
 
 @login_required(login_url='/authentication/login')
 def index(request):
-    return render(request, 'expenses/index.html')
+    categories = Category.objects.all()
+    context = {"categories": categories}
+    return render(request, 'expenses/index.html', context)
 
 def add_expense(request):
-    return render(request, "expenses/add_expense.html")
+    categories = Category.objects.all()
+    context = {
+        "categories": categories,
+        "values": request.POST
+    }
+
+    if request.method == "POST":
+        amount = request.POST.get('amount')
+        description = request.POST.get('description')
+
+        if not amount:
+            messages.error(request, "Amount is required")
+            return render(request, "expenses/add_expense.html", context)
+        if not description:
+            messages.error(request, "Description is required")
+            return render(request, "expenses/add_expense.html", context)
+
+    return render(request, "expenses/add_expense.html", context)
+
